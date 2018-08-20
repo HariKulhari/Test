165358701de409313d18148d3876d29b524d6041
hari_choudhary <hari.coretechies@gmail.com>
2018-08-20 17:46:00 +0530

    Initial commit

---
+++ utils/src/main/java/com/demo/app/utils/Maths.java
@@ -0,0 +1,13 @@
+package com.demo.app.utils;
+
+/**
+ * Created by Hari Choudhary on 8/20/2018.
+ * Core techies
+ * hari@coretechies.org
+ */
+public class Maths {
+
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+}
